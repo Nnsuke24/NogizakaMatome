92a8aaf9d8cb60def7d87db4d4da66904dad27ce
Nnsuke24 <nnsuke24@gmail.com>
2018-07-17 02:53:05 +0900

    TabBarとCollectionViewControllerの追加

---
+++ NogizakaMatome/Controller/MemberCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MemberCollectionViewCell.swift
+//  NogizakaMatome
+//
+//  Created by 福田光祐 on 2018/07/17.
+//  Copyright © 2018年 福田光祐. All rights reserved.
+//
+
+import UIKit
+
+class MemberCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var memberCell: UILabel!
+}
